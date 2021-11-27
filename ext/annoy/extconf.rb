e50cbd305fdc106f40c0fc83dcf0033379f35b55
yoshoku <yoshoku@outlook.com>
2021-11-28 00:53:02 +0900

    :boom: Remove cxxflags for optimization and multithreading

--- ext/annoy/extconf.rb
+++ ext/annoy/extconf.rb
@@ -2,7 +2,7 @@ require 'mkmf'
 
 abort 'libstdc++ is not found.' unless have_library('stdc++')
 
-$CXXFLAGS << " -std=c++14 -march=native -DANNOYLIB_MULTITHREADED_BUILD"
+$CXXFLAGS << " -std=c++14"
 $INCFLAGS << " -I$(srcdir)/src"
 $VPATH << "$(srcdir)/src"
 
