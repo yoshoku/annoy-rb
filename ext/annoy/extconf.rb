d0ee293f85df06cf9b2f8a61f070797f7425388e
yoshoku <yoshoku@outlook.com>
2026-02-17 08:34:15 +0900

    chore: remove unnecessary c++ library loading check

--- ext/annoy/extconf.rb
+++ ext/annoy/extconf.rb
@@ -1,17 +1,6 @@
 require 'mkmf'
 
-IS_MSWIN = /mswin/ =~ RUBY_PLATFORM
-IS_DARWIN = /darwin/ =~ RUBY_PLATFORM
-
-if IS_DARWIN
-  warn 'libc++ is not found.' unless have_library('c++')
-end
-
-if !IS_DARWIN && !IS_MSWIN
-  warn 'libstdc++ is not found.' unless have_library('stdc++')
-end
-
-if IS_MSWIN
+if /mswin/ =~ RUBY_PLATFORM
   $CXXFLAGS << ' /std:c++17'
 else
   $CXXFLAGS << ' -std=c++14'
